8968caa4dbd53c39fdd43f8c41ca94af69db1dd6
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-03-26 17:55:58 +0300

    Initial Commit

---
+++ RegistrationHW2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistrationHW2.3
+//
+//  Created by Максим Гурков on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
