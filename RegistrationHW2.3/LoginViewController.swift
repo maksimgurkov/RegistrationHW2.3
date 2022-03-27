a8861d151e6c6238239fcfb2110b51e87f2013bb
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-03-27 16:21:11 +0300

    Finish project

--- RegistrationHW2.3/LoginViewController.swift
+++ RegistrationHW2.3/LoginViewController.swift
@@ -9,16 +9,24 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    let name = "Max"
-    let password = "12345"
-    let error = "Pleas enter correct login and password"
+    private let name = "Max"
+    private let password = "12345"
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        nameTextField.delegate = self
+        passwordTextField.delegate = self
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        nameTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -35,43 +43,56 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func logInAction(_ sender: Any) {
-        
-        if nameTextField.text == name && passwordTextField.text == password {
-            
-        } else {
-            testAklert()
-        }
-        
+        guard nameTextField.text != name || passwordTextField.text != password else { return }
+        errorLogIn()
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
         guard segue.source is WelcomeViewController else { return }
-        nameTextField.text = nil
         passwordTextField.text = nil
+        nameTextField.text = nil
     }
     
-   private func remindInfo(fo info: String) {
-        let alert = UIAlertController(title: "Info",
-                                      message: "\(info)",
-                                      preferredStyle: .alert)
-        let alertAction = UIAlertAction(title: "ok", style: .default)
-        alert.addAction(alertAction)
-        present(alert, animated: true)
-    }
-    
-    private func testAklert() {
-        
-        let alertt = UIAlertController(title: "", message: "", preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "", style: .default) { _ in
-            self.passwordTextField.text = nil
-        }
-        alertt.addAction(action)
-        present(alertt, animated: true)
-        
-    }
-    
+}
 
+//MARK: - Extension
+extension LoginViewController {
+    
+    private func remindInfo(fo info: String) {
+         let alert = UIAlertController(title: "Info",
+                                       message: "\(info)",
+                                       preferredStyle: .alert)
+         let alertAction = UIAlertAction(title: "ok", style: .default)
+         alert.addAction(alertAction)
+         present(alert, animated: true)
+     }
+     
+     private func errorLogIn() {
+         
+         let alert = UIAlertController(
+            title: "Error",
+            message: "Pleas enter correct login and password",
+            preferredStyle: .alert
+         )
+         
+         let alertAction = UIAlertAction(title: "OK", style: .default) { _ in
+             self.passwordTextField.text = nil
+         }
+         alert.addAction(alertAction)
+         present(alert, animated: true)
+         
+     }
+    
+}
 
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        passwordTextField.becomeFirstResponder()
+    
+        return true
+    }
+    
 }
 
